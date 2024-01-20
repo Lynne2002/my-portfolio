f9b75f3910f5653eeb4eaad040bcdeaeb9ab12b6
Lynne Chepkwony <lynnebebet2@gmail.com>
2024-01-20 10:52:53 +0300

    feat(js): add script for about tabs

--- script.js
+++ script.js
@@ -25,3 +25,16 @@ var typed= new Typed(".typing", {
     backSpeed:60,
     loop:true
     });
+/*-------------------------About Tabs---------------------- */
+const tabsContainer = document.querySelector(".about-tabs"),
+aboutSection = document.querySelector(".about-section");
+
+tabsContainer.addEventListener("click", (e)=>{
+    if(e.target.classList.contains("tab-item")&&!e.target.classList.contains("active")){
+        tabsContainer.querySelector(".active").classList.remove("active");
+        e.target.classList.add("active");
+        const target= e.target.getAttribute("data-target");
+        aboutSection.querySelector(".tab-content.active").classList.remove("active");
+        aboutSection.querySelector(target).classList.add("active");
+    }
+});
