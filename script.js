aebda8d3e6362154eb53a02feee73a8194f0ad80
Lynne Chepkwony <lynnebebet2@gmail.com>
2024-01-20 14:58:45 +0300

    feat(js): add script for progress line
    for changing progress line according to data-progress percentage

--- script.js
+++ script.js
@@ -38,3 +38,19 @@ tabsContainer.addEventListener("click", (e)=>{
         aboutSection.querySelector(target).classList.add("active");
     }
 });
+
+/*-------------------------Skills---------------------- */
+const skills_wrap = document.querySelector(".about-skills"),
+    skills_bar = document.querySelectorAll(".progress-line");
+window.addEventListener("scroll", () => {
+    skillsEffect();
+})
+function checkScroll(el) {
+    let rect = el.getBoundingClientRect();
+    if (window.innerHeight >= rect.top + el.offsetHeight) return true;
+    return false;
+}
+function skillsEffect() {
+    if (!checkScroll(skills_wrap)) return;
+    skills_bar.forEach((skill) => (skill.style.width = skill.dataset.progress));
+}
